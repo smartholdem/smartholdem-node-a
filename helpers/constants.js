d2f375272c212f432d9e2a56f5d288a90fc6e970
technolog <viktor@allz.su>
2017-12-24 05:51:13 +0300

    milestones

--- helpers/constants.js
+++ helpers/constants.js
@@ -31,13 +31,13 @@ module.exports = {
 	requestLength: 104,
 	rewards: {
 		milestones: [
-			200000000, // Initial Reward
-			200000000, // Milestone 1
-			200000000, // Milestone 2
-			200000000, // Milestone 3
-			200000000  // Milestone 4
+			100000000, // Initial Reward
+			100000000, // Milestone 1
+			100000000, // Milestone 2
+			100000000, // Milestone 3
+			100000000  // Milestone 4
 		],
-		offset: 756000000,   // Start rewards at block, ie 7 days after net start
+		offset: 75600,   // Start rewards at block, ie 7 days after net start
 		distance: 3000000, // Distance between each milestone
 	},
 	signatureLength: 196,
