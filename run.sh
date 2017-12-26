1ba448c25e5ffeefd83c6f5c8993b8710d499bd4
technolog <viktor@allz.su>
2017-12-26 03:13:57 +0300

    testnet update script run

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash 
-forever start app.js --config config.smartholdem.json --genesis genesisBlock.smartholdem.json
+forever start app.js --config config.testnet.json --genesis genesisBlock.testnet.json
+tail -f ./logs/sth.log
