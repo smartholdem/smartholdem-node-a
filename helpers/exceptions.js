d26b6cf9d8b213870a0c556ea2885ee7c37768f0
technolog <viktor@allz.su>
2018-01-04 21:37:10 +0300

    add 1 vote from 1 addr

--- helpers/exceptions.js
+++ helpers/exceptions.js
@@ -6,7 +6,7 @@ module.exports = {
 	signatures: [
 	],
 	votes: [
-		"gendelegate_37"
+		"b620055ae2f5ea87e18ada2140734f08c0f84a8ea5f1b580752d54c021b3aa91"
 	],
 	balance:[
 		"070f2ece0fa3816f5c82b4b9bcf8b9d52546f6f0c226bee62ad4f89a98683dd7"
