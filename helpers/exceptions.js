9e3d3fa8e27fb974860c7864ca118cd6f382385c
technolog <viktor@allz.su>
2018-01-04 21:39:13 +0300

    add 1 vote from 1 addr

--- helpers/exceptions.js
+++ helpers/exceptions.js
@@ -2,8 +2,10 @@
 
 module.exports = {
 	senderPublicKey: [
+        "031b9e1b940a23fefa48849d8ee912dc3a46509adefb7e344d601af2a29a0783e9"
 	],
 	signatures: [
+        "3044022026ad61271ddae430dba38c985035d3923e3bb2d03bcfe5c0a30a525bee44c8a80220732639f8a24ecae17c6eae54b8f1294d729d452cd243eb6087a976cd26244d5b"
 	],
 	votes: [
 		"165009c7047d8cdc88f86318c028e7e7f340371eb4f24bede843c7fcf44eaeef"
