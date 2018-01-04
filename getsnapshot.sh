9c4a1059ba45089ac46dbaf323ad5c6688957911
technolog <viktor@allz.su>
2018-01-05 01:29:08 +0300

    load blockchain last snapshot

--- getsnapshot.sh
+++ getsnapshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 forever stop app.js
-wget http://explorer.smartholdem.io/snapshots/sth_smartholdem.191200
+wget http://explorer.smartholdem.io/snapshots/sth_smartholdem.latest
 dropdb sth_smartholdem
 createdb sth_smartholdem
 psql sth_smartholdem < sth_smartholdem.191200
