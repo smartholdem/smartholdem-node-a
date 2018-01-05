3db9a4e20dab8621743043fe35820bf25281dbcf
technolog <viktor@allz.su>
2018-01-06 00:23:45 +0300

    route forge

--- helpers/exceptions.js
+++ helpers/exceptions.js
@@ -2,7 +2,8 @@
 
 module.exports = {
 	senderPublicKey: [
-		'03ed93fae05bca542451825063042486c62af714e95db929dbdae6dde3a1b5809a'
+		'03ed93fae05bca542451825063042486c62af714e95db929dbdae6dde3a1b5809a',
+		'03302fdd67d91b1a7ac58f9a874ea43b500bf343466b8752b0f210441c6019b9c3'
 	],
 	signatures: [
 	],
