556c1b361e4afd67c64a778206cf03ab5e8d9652
technolog <viktor@allz.su>
2018-01-05 16:56:11 +0300

    exceptions

--- modules/delegates.js
+++ modules/delegates.js
@@ -394,7 +394,7 @@ __private.checkDelegates = function (publicKey, votes, state, cb) {
             var lastBlockV = modules.blockchain.getLastBlock()
             // 1 vote patch
 
-            if (lastBlockV.heigh > 194000) {
+            if (lastBlockV.height > 194000) {
 
             if (math === '+' && (existing_votes > 0)) {
                 library.logger.info('--- Only 1 vote from 1 address', 'height:' + lastBlockV.height);
