4fa77f8003fafef7ba37564e90ad6353a4ae8ffc
technolog <viktor@allz.su>
2018-03-13 23:11:52 +0300

    tests signatures

--- test/index.js
+++ test/index.js
@@ -9,10 +9,10 @@ require('./api/peer.transactions.delegates.js');
 require('./api/peer.transactions.js');
 require('./api/peer.transactions.signatures.js');
 require('./api/peer.transactions.votes.js');
+*/
 require('./api/peers.js');
 require('./api/signatures.js');
 require('./api/transactions.js');
-*/
 
 require('./unit/helpers/request-limiter.js');
 require('./unit/logic/blockReward.js');
