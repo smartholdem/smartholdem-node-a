0601362a2f593c1667c0bd32afbb15b34bff774c
technolog <viktor@allz.su>
2018-03-13 23:19:25 +0300

    tests index

--- test/index.js
+++ test/index.js
@@ -5,11 +5,9 @@ require('./api/loader.js');
 require('./api/multisignatures.js');
 require('./api/peer.js');
 require('./api/peer.transactions.delegates.js');
-/*
 require('./api/peer.transactions.js');
 require('./api/peer.transactions.signatures.js');
 require('./api/peer.transactions.votes.js');
-*/
 require('./api/peers.js');
 require('./api/signatures.js');
 require('./api/transactions.js');
