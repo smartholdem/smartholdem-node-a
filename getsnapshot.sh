38900867011fd5dcc0184863763d9d31ba36f1f3
technolog <viktor@allz.su>
2018-04-30 02:41:12 +0300

    remove old script

--- getsnapshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-forever stop app.js
-rm current
-wget http://explorer.smartholdem.io/snapshots/current
-dropdb sth_smartholdem
-createdb sth_smartholdem
-psql sth_smartholdem < current
-forever start app.js
-tail -f ./logs/sth.log
