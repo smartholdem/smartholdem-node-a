ac1bfbfa3c2a2cb7107a1c35319fae6f3e2a0da1
technolog <viktor@allz.su>
2018-04-30 01:07:57 +0300

    secret json

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 forever stop app.js
 git fetch && git merge
-forever start app.js --config config.devnet.json --genesis genesisBlock.devnet.json
+TOP=true forever start app.js --config config.devnet.json --genesis genesisBlock.devnet.json
 tail -f ./logs/sth.log
