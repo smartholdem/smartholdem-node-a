53ffeda521a8162f230da90ef9dc6924e8002cdc
technolog <viktor@allz.su>
2018-04-30 02:24:41 +0300

    fast first start

--- firststart.sh
+++ firststart.sh
@@ -1,9 +1,14 @@
 forever stop app.js
 rm snapshot_dev.zip
 rm snapshot_dev
+echo 'Getting last snapshot..'
 wget http://snapshots.smartholdem.io/snapshot_dev.zip
+unzip snapshot_dev.zip
 dropdb sth_devnet
 createdb sth_devnet
+echo 'DB recreate!'
+echo 'Import Dump Blockchain..'
 psql sth_devnet < snapshot_dev
+echo 'Success!'
 TOP=true forever start app.js --config config.devnet.json --genesis genesisBlock.devnet.json
 tail -f ./logs/sth.log
