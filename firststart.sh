0f6edc7490ddda217e10d32c0ea1f7d8a18585df
technolog <viktor@allz.su>
2018-04-30 02:58:11 +0300

    remove old script

--- firststart.sh
+++ firststart.sh
@@ -1,14 +1,17 @@
 forever stop app.js
 echo 'Getting last snapshot..'
-wget http://snapshots.smartholdem.io/snapshot.zip
-unzip snapshot.zip
+#wget http://snapshots.smartholdem.io/snapshot.zip
+wget http://snapshots.smartholdem.io/snapshot
+#unzip snapshot.zip
 dropdb sth_smartholdem
 createdb sth_smartholdem
 echo 'DB recreate!'
 echo 'Import Dump BlockChain..'
-psql sth_smartholdem < snapshot_last
+#psql sth_smartholdem < snapshot_last
+pg_restore -d sth_smartholdem --role=$USER snapshot
 echo 'Success!'
-rm snapshot.zip
-rm snapshot_last
+#rm snapshot.zip
+#rm snapshot_last
+rm snapshot
 forever start app.js --config config.smartholdem.json --genesis genesisBlock.smartholdem.json
 tail -f ./logs/sth.log
