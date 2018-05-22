addf53b79823dd6d5ce116fac23c8b832a3f9e0f
technolog <viktor@allz.su>
2018-05-22 14:26:52 +0300

    autoupdate crontab script

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 forever stop app.js
+sudo apt-get update -y
 git fetch && git merge
+git pull
+npm install -g npm forever grunt-cli
 npm install
 forever start app.js --config config.smartholdem.json --genesis genesisBlock.smartholdem.json
