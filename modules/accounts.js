4358f336447559529f129fe3825fad1b9ce9eee6
technolog <viktor@allz.su>
2018-11-18 07:36:01 +0300

    remove old tests

--- modules/accounts.js
+++ modules/accounts.js
@@ -86,7 +86,7 @@ __private.attachApi = function () {
 	}
 
 	router.get('/count', function (req, res) {
-		console.log(__private.accounts);
+        library.logger.info(__private.accounts);
 		return res.json({success: true, count: Object.keys(__private.accounts).length});
 	});
 
