c52b8a9b70bec75df3a6806f7b0ae5cb77b8c6ba
technolog <viktor@allz.su>
2018-11-20 05:29:19 +0300

    cleanup logs

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 forever stop app.js
+forever cleanlogs
 git fetch && git merge
 TOP=true forever start app.js --config config.devnet.json --genesis genesisBlock.devnet.json
 tail -f ./logs/sth.log
