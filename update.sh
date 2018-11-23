c6a5efa1a11d1cd4581089986851129bdc084606
technolog <viktor@allz.su>
2018-11-23 03:42:37 +0300

    up shell

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 forever stop app.js
 forever cleanlogs
 git fetch && git merge
-forever start -c "node --stack-size=65500" app.js --config config.json --genesis genesisBlock.json
+forever start app.js --config config.json --genesis genesisBlock.json
 tail -f ./logs/sth.log
