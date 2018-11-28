f073733d484b616c3e609abe26072de42ec5a012
technolog <viktor@allz.su>
2018-11-28 19:40:57 +0300

    remove old big log after update shell

--- modules/peers.js
+++ modules/peers.js
@@ -110,7 +110,7 @@ __private.updatePeersList = function (cb) {
 
 			var peers = shuffle(
 							res.body.peers
-								.filter(peer => peer.ip.substr(0,3) != "127") // exclude loopback addresses
+								.filter(peer => peer.ip.substr(0,3) !== "127") // exclude loopback addresses
 						) // randomize the list to prevent malicious list crafting
 						.slice(0, reach); // don't query everyone - that would be spam
 
--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 forever stop app.js
 forever cleanlogs
+rm ./logs/sth.log
+touch ./logs/sth.log
 git fetch && git merge
 forever start app.js --config config.json --genesis genesisBlock.json >&- 2>&-
 tail -f ./logs/sth.log
