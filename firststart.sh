cbd02bacadcddeff734a1b5d00037eaa7f60b43d
technolog <viktor@allz.su>
2018-11-30 20:05:53 +0300

    shell script update

--- firststart.sh
+++ firststart.sh
@@ -5,7 +5,7 @@ dropdb sth_smartholdem
 createdb -E UTF8 -T template0 --lc-collate=en_US.UTF-8 --lc-ctype=en_US.utf8 sth_smartholdem
 echo 'DB recreate!'
 echo 'Loading Full BlockChain, please wait..'
-pg_restore -d sth_smartholdem --role=$USER snapshot >&- 2>&-
+pg_restore --no-acl --role=$USER -d sth_smartholdem snapshot >&- 2>&-
 echo 'Success!'
 rm snapshot
 forever start app.js --config config.json --genesis genesisBlock.json >&- 2>&-
