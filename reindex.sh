9700446a9ce772777419ab0b8ca43a0cc6f6cc09
technolog <viktor@allz.su>
2019-01-14 15:40:14 +0300

    shell script optimize database

---
+++ reindex.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sh stop.sh
+psql "postgresql://$USER:password@127.0.0.1/sth_smartholdem" -c 'REINDEX DATABASE sth_smartholdem'
+psql "postgresql://$USER:password@127.0.0.1/sth_smartholdem" -c 'vacuum full analyze'
+sh run.sh
