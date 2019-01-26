5d33bce319076885493e82a4989c92f018cd3888
technolog <viktor@allz.su>
2019-01-26 05:35:42 +0300

    shell script optimize database

--- reindex.sh
+++ reindex.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "Stopping node"
 sh stop.sh
-echo "BlockChain Optimize Started please wait.."
+echo "SmartHoldem BlockChain Optimize Started please wait.."
 psql "postgresql://$USER:password@127.0.0.1/sth_smartholdem" -c 'REINDEX DATABASE sth_smartholdem'
 psql "postgresql://$USER:password@127.0.0.1/sth_smartholdem" -c 'vacuum full analyze'
+echo "Optimization Success! Start BlockChan.."
 sh run.sh
